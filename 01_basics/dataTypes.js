6aac738b5e7f8bbf770ce09572c1bdc91620d0fb
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2023-10-18 06:33:48 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version, each code code is treated as newer version.
+//puri file mein code naye version ki tarah treat kiya jaayega
+
+// alert(3+3) // cannot use alert as we are usig node, not the browser
+console.log(3+5)
+console.log("Aditya")
+//code readability should be high
+//to read docs we access from Mdn or tc39 for original
+
+let name ="Aditya" //string
+let age = 20 //number
+let isLoggedIn=false //boolean
+
+//DATA TYPES IN JS
+
+//number => range 
+//bigint
+//string =>""
+//boolean => true/false
+//null //this is a data type as well as a standalone value also.
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "Aditya")
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("Aditya")
+const accountId = 210222164
+let accountEmail = "xyz@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+// accountId=2 //not allowed
+console.log(accountId)
+accountEmail="asj@gmail.com"
+accountPassword="414312"
+accountCity="Greater Noida"
+
+//instead of using console log every time for different variable printing, we can use console table
+console.table([accountId,accountEmail,accountPassword,accountCity])
