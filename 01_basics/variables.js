7e987b31a04ad08fc92f15696e75edaf2a6ba433
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2024-04-21 13:33:16 +0000

    updated variable.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,7 +1,8 @@
 const accountId = 210222164
 let accountEmail = "xyz@google.com"
 var accountPassword="12345"
-accountCity="Delhi"
+accountCity="Delhi" // we can also create variables like this in JS, but it is not a recommended way to make variables
+
 // accountId=2 //not allowed
 console.log(accountId)
 accountEmail="asj@gmail.com"
@@ -11,6 +12,12 @@ let accountState;
 
 //instead of using console log every time for different variable printing, we can use console table
 console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
 /*
 prefer not to use var, because of issues in block scope and functional scope
+*/
+
+
+/*
+Another way of making comments in our code files
 */
