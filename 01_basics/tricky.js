44e5b9b6c73bee6d28f79db6db79eba4939322b9
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2024-04-21 17:27:16 +0000

    tricky.js(updt)

--- 01_basics/tricky.js
+++ 01_basics/tricky.js
@@ -3,7 +3,21 @@ str2=" Shubham"
 str3= str1+str2;
 // console.log(str3); concatenate two strings 
 
-console.log("1" + 2); //12
-console.log(1 + "2"); //12
-console.log("1" + 2 + 2); //122
-console.log(1 + 2 + "2"); //32 isme pehle 1+2 ho gaya hai fir string mein convert and concatenate
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32 isme pehle 1+2 ho gaya hai fir string mein convert and concatenate
+
+// console.log(+true);//1, true is 1 and we are trying to increment true so it gives 1 as output.
+// console.log(Boolean("")); //"" converted to boolean is false and if we try to increase false it will give 0.
+
+let num1, num2, num3
+num1=num2=num3= 2+2 // bad code, although it works but then even it is not good way to write code
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+let x=3;
+let y=x++;
+console.log("x is",x + " y is " ,y);
