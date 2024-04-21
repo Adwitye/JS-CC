02987e4e1aa2e551c87ff3f471f64969a26f3f1f
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2024-04-21 14:31:02 +0000

    updated conversionOps

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,25 +1,38 @@
 let score = "33"
 
 // const {score} = req.body
-// console.log(typeof score);
-// console.log(typeof(score));
+// console.log(typeof score); 
+// console.log(typeof(score)); we can also write it as a method typeof() 
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let a="10abc"
+let valueInNumber = Number(a) //type conversion
+console.log(typeof valueInNumber); // converted '10abc' into number 
+console.log(valueInNumber); //NaN
 
-//"33" =>33
+//string converted into number(written below)
+//"33" => 33
 // "33abc" => NaN
 //true =>1; false=>0
 
 let isLoggedIn = "aditya"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
+
+//converted into boolean (written below)
 // 1=> true; 0=>false
-// ""=> false
+// ""=> false,  empty string if converted to boolean gives false as value
 // "aditya"=>true
 
 let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//typeof(NaN) is Number
+//typeof(null) is object
+
+let b=null
+console.log(typeof(b)); // object,  typeof(null) is object
+let c= Number(b); //null converted into 'Number', it becomes 0
+console.log(c); // 0
+console.log(typeof c);
