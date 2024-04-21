f2765116a0f9d5ed69d55709c8c6aa73959b73fb
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2024-04-21 15:01:49 +0000

    tricky concepts

---
+++ 01_basics/tricky.js
@@ -0,0 +1,9 @@
+str1="Aditya"
+str2=" Shubham"
+str3= str1+str2;
+// console.log(str3); concatenate two strings 
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32 isme pehle 1+2 ho gaya hai fir string mein convert and concatenate
